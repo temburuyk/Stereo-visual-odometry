d1179685f9600d5f8f012c0e525197a72ebe3b34
temburuyk <temburuyk@gmail.com>
2019-07-03 08:23:24 +0000

    Ovelay bitstream loading to PL and able to get result from dram

--- pynq_overlay_src/pynq_ip_1.py
+++ pynq_overlay_src/pynq_ip_1.py
@@ -1,4 +1,7 @@
 from pynq import Overlay
+from pynq import Xlnk
+import numpy as np
+
 
 overlay = Overlay('/home/xilinx/SVO/Stereo-visual-odometry/design_1.bit')
 
@@ -8,6 +11,47 @@ mul_ip = overlay.mat_multiply_0
 
 mul_ip
 
-#mul_ip.register_map
+mul_ip.register_map
+
+print(overlay.ip_dict.keys())
 
 mul_ip.read(0x0c)
+
+xlnk = Xlnk()
+
+A_buffer = xlnk.cma_array(shape=(9,), dtype=np.ubyte)
+B_buffer = xlnk.cma_array(shape=(9,), dtype=np.ubyte)
+C_buffer = xlnk.cma_array(shape=(9,), dtype=np.ubyte)
+
+A_dram_addr = A_buffer.physical_address
+B_dram_addr = B_buffer.physical_address
+C_dram_addr = C_buffer.physical_address
+
+print(hex(A_dram_addr)) 
+
+A_reg_offset = mul_ip.register_map.A.address
+B_reg_offset = mul_ip.register_map.B.address
+C_reg_offset = mul_ip.register_map.C.address
+CTRL_reg_offset = mul_ip.register_map.CTRL.address
+
+for i in range(9):
+	A_buffer[i]=i
+	B_buffer[i]=8-i
+
+mul_ip.write(A_reg_offset,A_dram_addr)
+mul_ip.write(B_reg_offset,B_dram_addr)
+mul_ip.write(C_reg_offset,C_dram_addr)
+
+mul_ip.write(CTRL_reg_offset,1)
+cycles = 0
+#print(mul_ip.register_map.CTRL)
+#print(mul_ip.register_map.CTRL.AP_START)
+#print(mul_ip.register_map.CTRL.AP_DONE)
+while(not(mul_ip.register_map.CTRL.AP_DONE)):
+	print(cycles)
+	cycles = cycles+1
+
+print(A_buffer)
+print(B_buffer)
+print(C_buffer)
+
